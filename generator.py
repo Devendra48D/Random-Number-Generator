53601ad339bb24437883424d7482d3ae6fe63699
Devendra Shah <devendrashah97@gmail.com>
2017-09-15 17:49:38 -0400

    taking user input

--- generator.py
+++ generator.py
@@ -21,19 +21,23 @@ def exponential_additive():
 def exponential_quadratic():
 	pass
 
-distribution_choice = input("Which distribution would you like to choose to generate the random numbers? Enter 1 for uniform and 2 for exponential distribution. : ")
+distribution_choice = input("Which distribution would you like to choose to generate the random numbers?" +
+	"Enter 1 for uniform and 2 for exponential distribution. : ")
 
 while (distribution_choice != 1 and distribution_choice != 2):
 	print ""
 	print "Please enter a valid choice."
-	distribution_choice = input("Which distribution would you like to choose to generate the random numbers? Enter 1 for uniform and 2 for exponential distribution. : ")
+	distribution_choice = input("Which distribution would you like to choose to generate the random numbers?" 
+		" Enter 1 for uniform and 2 for exponential distribution. : ")
 
 print ""
-technique_choice = input("What technique would you like to choose to generate the random numbers? Enter 1 for LCG, 2 for additive, and 3 for quadratic technique. : ")
+technique_choice = input("What technique would you like to choose to generate the random numbers?" +
+	"Enter 1 for LCG, 2 for additive, and 3 for quadratic technique. : ")
 while (technique_choice != 1 and technique_choice != 2 and technique_choice != 3):
 	print ""
 	print "Please enter a valid choice."
-	technique_choice = input("What technique would you like to choose to generate the random numbers? Enter 1 for LCG, 2 for additive, and 3 for quadratic technique. : ")
+	technique_choice = input("What technique would you like to choose to generate the random numbers?" + 
+	 "Enter 1 for LCG, 2 for additive, and 3 for quadratic technique. : ")
 
 
 print "distribution_choice: " + str(distribution_choice) + " technique_choice: " + str(technique_choice)
