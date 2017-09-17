9bacc0efedd4a61788926dcf5e77b8f93bf9343f
Devendra Shah <devendrashah97@gmail.com>
2017-09-17 18:34:01 -0400

    files updated and some bugs fixed

--- generator.py
+++ generator.py
@@ -2,127 +2,162 @@ import math
 print "Random Number Generator"
 
 #Using the formal Z(i) = (a * Z(i-1) + c) mod m
-def uniform_LCG():
+def uniform_LCG(iterations):
 	m = float(2 ** 32)
 	a = 22695477
 	c = 1
 	#seed the random number value at 1
 	currval = float(1)
 	i = 0
-	iterations = 10000
 	outputFile = open("Shah_Devendra_uniform_LCG.csv", "wb")
 	while i < iterations:
 		currval = (a * currval + c) % m
-		outputFile.write(str(currval/m) + "\n")
+		outputFile.write(str(currval/m) + ",\n")
 		i += 1
 	outputFile.close()
-	print "10000 random numbers generated and stored in Shah_Devendra_uniform_LCG.csv."
+	print str(iterations) + " random numbers generated and stored in Shah_Devendra_uniform_LCG.csv."
 
-#Using the formula Z(i) = (Z(i-k) + Z(i-j)) mod m 
-def uniform_additive():
+#Using the formula 
+def uniform_additive(iterations):
 	m = float(2 ** 32)
-	k = 3
-	j = 1
+	j = 24
+	k = 55
 	#seed the random number value at 1
-	currval = float(1)
-	dp = [0 for _ in range(10000 + k)]
-	for i in range(10000 + k):
-		if i < 3:
-			dp[i] = currval
+	currvalodd = float(2 ** 30 - 1)
+	currvaleven = float(2 ** 30)
+	dp = [0 for _ in range(iterations + k + 1500)]
+	for i in range(iterations + k + 1500):
+		if i < k:
+			if i % 2 == 1:
+				dp[i] = currvalodd
+			else:
+				dp[i] = currvaleven
 		else:
 			dp[i] = (dp[i - j] + dp[i - k]) % m
 	i = 0
-	iterations = 10000
 	outputFile = open("Shah_Devendra_uniform_additive.csv", "wb")
-	while i < iterations:
-		outputFile.write(str(dp[i+k]/m) + "\n")
+	while i < iterations + 1500:
+		if i >= 1500:
+			outputFile.write(str(dp[i+k]/m) + ",\n")
 		i += 1
 	outputFile.close()
-	print"10000 random numbers generated and stored in Shah_Devendra_uniform_additive.csv."
+	print str(iterations) + " random numbers generated and stored in Shah_Devendra_uniform_additive.csv."
 
-#Using the formula Z(i) = (a1*(Z(i-1))**2 + a2*Z(i-1) + c) mod m
-def uniform_quadratic():
-	a1 = 22695477
-	a2 = 134775813
+#Using the formula X(n+1) = (d*(X(n) ** 2) + a*X(n) + c) mod m
+#tried this does not work, seems to loop over same values 
+#in just 5-6 values
+#ignore this
+def uniform_quadratic_1_faulty(iterations):
+	a = 22695477
+	d = 134775812
 	c = 1
 	m = float(2 ** 32)
 	#seed the random number value at 1
 	currval = float(1)
 	i = 0
-	iterations = 10000
 	outputFile = open("Shah_Devendra_uniform_quadratic.csv", "wb")
 	while i < iterations:
-		currval = (a1 * (currval ** 2) + a2 * currval + c) % m
-		outputFile.write(str(currval/m) + "\n")
+		currval = (d * (currval ** 2) + a * currval + c) % m
+		outputFile.write(str(currval/m) + ",\n")
 		i += 1
 	outputFile.close()
-	print"10000 random numbers generated and stored in Shah_Devendra_uniform_quadratic.csv."
+	print str(iterations) + "random numbers generated and stored in Shah_Devendra_uniform_quadratic.csv."
+
+#working version
+#used this instead for quadratic 
+#X(0)mod 4 = 2 and X(n+1) = X(n)*(X(n) + 1)mod 2e
+def uniform_quadratic(iterations):
+	m = float(2 ** math.e)
+	#seed the random number value at 10
+	currval = float(10)
+	i = 0
+	outputFile = open("Shah_Devendra_uniform_quadratic.csv", "wb")
+	while i < iterations:
+		currval = (currval * (currval+1)) % m
+		outputFile.write(str(currval/m) + ",\n")
+		i += 1
+	outputFile.close()
+	print str(iterations) + " random numbers generated and stored in Shah_Devendra_uniform_quadratic.csv."
 
 #function to convert non zero Uniform distribution random numbers to exponential distribution
 def uniform_to_exponential(ui, lmbd):
 	if ui == 0:
 		return 0
-	return -1 * (float(1)/lmbd) * math.log(ui)
-
+	return (-1 * (float(1)/lmbd) * math.log(ui, math.e)) % 1
 
 #use uniform_to_exponential function to convert uniform distribution to exponential distribution
-def exponential_LCG():
+def exponential_LCG(iterations):
 	m = float(2 ** 32)
 	a = 22695477
 	c = 1
-	lmbd = float(1)
+	arr = []
 	#seed the random number value at 1
 	currval = float(1)
 	i = 0
-	iterations = 10000
 	outputFile = open("Shah_Devendra_exponential_LCG.csv", "wb")
 	while i < iterations:
 		currval = (a * currval + c) % m
-		outputFile.write(str(uniform_to_exponential(currval/m, lmbd)) + "\n")
+		arr.append(currval/m)
 		i += 1
-	outputFile.close()
-	print "10000 random numbers generated and stored in Shah_Devendra_exponential_LCG.csv."
-
-#use uniform_to_exponential function to convert uniform distribution to exponential distribution
-def exponential_additive():
-	m = float(2 ** 32)
-	k = 3
-	lmbd = float(1)
-	#seed the random number value at 1
-	currval = float(1)
-	dp = [0 for _ in range(10000+k)]
-	for i in range(10000 + k):
-		if i < 3:
-			dp[i] = currval
-		else:
-			dp[i] = (dp[i - 1] + dp[i - k]) % m
 	i = 0
-	iterations = 10000
-	outputFile = open("Shah_Devendra_exponential_additive.csv", "wb")
+	lmbd = sum(arr)/float(len(arr))
 	while i < iterations:
-		outputFile.write(str(uniform_to_exponential(dp[i+k]/m, lmbd)) + "\n")
+		outputFile.write(str(uniform_to_exponential(arr[i], lmbd)) + ",\n")
 		i += 1
 	outputFile.close()
-	print"10000 random numbers generated and stored in Shah_Devendra_exponential_additive.csv."
+	print str(iterations) + " random numbers generated and stored in Shah_Devendra_exponential_LCG.csv."
 
 #use uniform_to_exponential function to convert uniform distribution to exponential distribution
-def exponential_quadratic():
-	a1 = 22695477
-	a2 = 134775813
-	c = 1
+def exponential_additive(iterations):
 	m = float(2 ** 32)
-	lmbd = float(1)
+	j = 24
+	k = 55
 	#seed the random number value at 1
-	currval = float(1)
+	currvalodd = float(2 ** 30 - 1)
+	currvaleven = float(2 ** 30)
+	dp = [0 for _ in range(iterations + k + 1500)]
+	for i in range(iterations + k + 1500):
+		if i < k:
+			if i % 2 == 1:
+				dp[i] = currvalodd
+			else:
+				dp[i] = currvaleven
+		else:
+			dp[i] = (dp[i - j] + dp[i - k]) % m
 	i = 0
-	iterations = 10000
+	outputFile = open("Shah_Devendra_exponential_additive.csv", "wb")
+	arr = []
+	while i < iterations + 1500:
+		if i >= 1500:
+			arr.append(dp[i+k]/m)
+		i += 1
+	lmbd = sum(arr)/float(len(arr))
+	i = 0
+	while i < iterations:
+		outputFile.write(str(uniform_to_exponential(arr[i], lmbd)) + ",\n")
+		i += 1
+	outputFile.close()
+	print str(iterations) + " random numbers generated and stored in Shah_Devendra_exponential_additive.csv."
+
+#use uniform_to_exponential function to convert uniform distribution to exponential distribution
+def exponential_quadratic(iterations):
+	m = float(2 ** math.e)
+	#seed the random number value at 10
+	currval = float(10)
+	i = 0
+	arr = []
 	outputFile = open("Shah_Devendra_exponential_quadratic.csv", "wb")
 	while i < iterations:
-		currval = (a1 * (currval ** 2) + a2 * currval + c) % m
-		outputFile.write(str(uniform_to_exponential(currval/m, lmbd)) + "\n")
+		currval = (currval * (currval+1)) % m
+		arr.append(currval/m)
+		i += 1
+	lmbd = sum(arr)/float(len(arr))
+	i = 0
+	while i < iterations:
+		outputFile.write(str(uniform_to_exponential(arr[i], lmbd)) + ",\n")
 		i += 1
 	outputFile.close()
-	print"10000 random numbers generated and stored in Shah_Devendra_exponential_quadratic.csv."
+	print str(iterations) + " random numbers generated and stored in Shah_Devendra_exponential_quadratic.csv."
 
 
 distribution_choice = input("Which distribution would you like to choose to generate the random numbers?" +
@@ -143,18 +178,30 @@ while (technique_choice != 1 and technique_choice != 2 and technique_choice != 3
 	technique_choice = input("What technique would you like to choose to generate the random numbers?" + 
 	 "Enter 1 for LCG, 2 for additive, and 3 for quadratic technique. : ")
 
+iteration_count = input("Please enter the number of random numbers you would like to generate." +
+	"Please enter a valid positive number. : ")
+while  iteration_count < 1 or isinstance(iteration_count, int) != True:
+	print ""
+	print "Please enter a valid number"
+	iteration_count = input("Please enter the number of random numbers you would like to generate." +
+	"Please enter a valid positive number. : ")
+	print ""
+
+
 
 print "distribution_choice: " + str(distribution_choice) + " technique_choice: " + str(technique_choice)
+print "Number of random numbers to generate: " + str(iteration_count)
+
 
 if distribution_choice == 1 and technique_choice == 1:
-	uniform_LCG()
+	uniform_LCG(iteration_count)
 elif distribution_choice == 1 and technique_choice == 2:
-	uniform_additive()
+	uniform_additive(iteration_count)
 elif distribution_choice == 1 and technique_choice == 3:
-	uniform_quadratic()
+	uniform_quadratic(iteration_count)
 elif distribution_choice == 2 and technique_choice == 1:
-	exponential_LCG()
+	exponential_LCG(iteration_count)
 elif distribution_choice == 2 and technique_choice == 2:
-	exponential_additive()
+	exponential_additive(iteration_count)
 elif distribution_choice == 2 and technique_choice == 3:
-	exponential_quadratic()
+	exponential_quadratic(iteration_count)
